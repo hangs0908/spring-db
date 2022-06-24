f7f34bbfec2fda373a219431d1ccfe4eba12acf1
hangs0908 <hangs0908@gmail.com>
2022-06-24 14:14:05 +0900

    Jpa 관련

--- src/main/java/hello/itemservice/domain/Item.java
+++ src/main/java/hello/itemservice/domain/Item.java
@@ -2,11 +2,16 @@ package hello.itemservice.domain;
 
 import lombok.Data;
 
+import javax.persistence.*;
+
 @Data
+@Entity
 public class Item {
 
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(name = "item_name") // 스프링부트를 사용하면 없어도 된다. 자동으로 캐멀 -> 언더스코어로 변경해준다 .
     private String itemName;
     private Integer price;
     private Integer quantity;
